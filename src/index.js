a57ed2abc4bad2d67fad458e9541cc90cb9738bd
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-02-27 18:12:38 +0530

    Created Header and content component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// React Bootstrap Configuration
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
